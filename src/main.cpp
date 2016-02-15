b71767269f0fc8460d07c4e2d64e67af7b898040
Lef Ioannidis <elefthei@mit.edu>
2016-02-15 00:08:12 -0500

    Fixed indentaiton

--- src/main.cpp
+++ src/main.cpp
@@ -24,68 +24,77 @@ int main(int argc, char* argv[]){
   char ckey1[key1size];
   char ckey2[key1size+1];
   
-  if(strlen(argv[1])%2==0){
-    for(int i=0;i<key1size;++i){
+  if (strlen(argv[1])%2==0) {
+    for (int i=0;i<key1size;++i) {
       ckey1[i]=argv[1][i];
       ckey2[i]=argv[1][key1size+i];
-	}
+		}
   }
-  else{
-    for(int i=0;i<key1size;++i){
+
+  else {
+    for (int i=0;i<key1size;++i){
       ckey1[i]=argv[1][i];
       ckey2[i]=argv[1][key1size+i];
 	}
     ckey2[key1size]=argv[1][2*key1size];
   }
-  double key1=(double)atoi(ckey1), key2=(double)atoi(ckey2);
-   if(key1==key2){perror("Key cannot be symmetrical! Use your imagination!\n"); return -1;}
-   //end of initializations of key1, key2
+  
+	double key1=(double)atoi(ckey1), key2=(double)atoi(ckey2);
+  if (key1==key2) { perror("Key cannot be symmetrical! Use your imagination!\n"); return -1; }
+  //end of initializations of key1, key2
    
    
-   double eskey1=pow(key1,2)*pow(10,((-1)*cdigits(pow(key1,2)))+1), 
-	  eskey2=pow(key2,2)*pow(10,((-1)*cdigits(pow(key2,2)))+1);
+  double eskey1=pow(key1,2)*pow(10,((-1)*cdigits(pow(key1,2)))+1), 
+	  	 	 eskey2=pow(key2,2)*pow(10,((-1)*cdigits(pow(key2,2)))+1);
 
-   Y1[0]=eskey1-floor(eskey1);
-   Y2[0]=eskey2-floor(eskey2);
-   //end initializations and checks
-   //eskey = key^2*10^(-keydigits+1)
-   FILE *fin,*fout;
-   
-   _byte c=0;
-   char *pch=strstr(argv[2],".hid");
-   char *outpfile;
-   if(pch==NULL){
+  Y1[0]=eskey1-floor(eskey1);
+  Y2[0]=eskey2-floor(eskey2);
+  //end initializations and checks
+  //eskey = key^2*10^(-keydigits+1)
+  FILE *fin,*fout;
 
-     int maxsize = MIN(strlen(argv[2])+4,MAX_PASSWD);
-     outpfile= (char*) calloc(maxsize,sizeof(char));
-     strncpy(outpfile,argv[2], maxsize);
-     strcat(outpfile,".hid");
-   }
-   else {
-     int maxsize = MIN(strlen(argv[2])-4,MAX_PASSWD);
-     outpfile= (char*) calloc(maxsize,sizeof(char));
-     for(int i = 0; i<maxsize; ++i)
+	_byte c=0;
+	char *pch=strstr(argv[2],".hid");
+	char *outpfile;
+	if(pch==NULL){
+
+	  int maxsize = MIN(strlen(argv[2])+4,MAX_PASSWD);
+	  outpfile= (char*) calloc(maxsize,sizeof(char));
+	  strncpy(outpfile,argv[2], maxsize);
+	  strcat(outpfile,".hid");
+  }
+
+  else {
+    int maxsize = MIN(strlen(argv[2])-4,MAX_PASSWD);
+    outpfile= (char*) calloc(maxsize,sizeof(char));
+    for(int i = 0; i<maxsize; ++i)
        outpfile[i]=argv[2][i];
-   }
+  }
 
-    if((fin=fopen(argv[2],"rb"))!=NULL){
-      if((fout=fopen(outpfile,"wb"))!=NULL){
-	free(outpfile);
-	while(!feof(fin)){
-	  c=getc(fin);
-	  c^=prbg(&eskey1,&eskey2);
-	  putc(c,fout);
-	}
-	fclose(fout);
-      }
-      else{
-	printf("No output file\n");
-      }
-      fclose(fin);
-    }
-    else{
-      printf("No input file\n");
+  if ((fin=fopen(argv[2],"rb"))!=NULL) {
+  	if ((fout=fopen(outpfile,"wb"))!=NULL) {
+			free(outpfile);
+
+			while (!feof(fin)) {
+	  		c=getc(fin);
+	  		c^=prbg(&eskey1,&eskey2);
+	  		putc(c,fout);
+			}
+
+			fclose(fout);
     }
 
-return 0;
+		else {
+			printf("No output file\n");
+    }
+
+    fclose(fin);
+  }
+
+  else {
+  	printf("No input file\n");
+  }
+
+  return 0;
 }
+
